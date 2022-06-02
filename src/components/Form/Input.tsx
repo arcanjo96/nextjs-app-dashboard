0c05d44d3a164a6a84820bd47e5d66d8858c39dd
Lucas Arcanjo <lucasarcanjo.ld@gmail.com>
2022-06-01 23:34:25 -0300

    feat: adds new pages: ListUsers and CreateUser. Adds new component: Pagination

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -13,7 +13,7 @@ interface InputProps extends ChakraInputProps {
 export function Input({ name, label, ...rest }: InputProps) {
   return (
     <FormControl>
-      {!!label && <FormLabel htmlFor={label}>E-mail</FormLabel>}
+      {!!label && <FormLabel htmlFor={label}>{label}</FormLabel>}
       <ChakraInput
         name={name}
         type="email"
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,46 @@
+import { Stack, Button, Box } from '@chakra-ui/react';
+
+export function Pagination() {
+  return (
+    <Stack
+      direction="row"
+      spacing="6"
+      mt="8"
+      justify="space-between"
+      align="center"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+
+      <Stack direction="row" spacing="2">
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          colorScheme="pink"
+          disabled
+          _disabled={{
+            bgColor: 'pink.500',
+            cursor: 'default',
+          }}
+        >
+          1
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          colorScheme="pink"
+          bg="gray.700"
+          _hover={{
+            bgColor: 'gray.500',
+            cursor: 'default',
+          }}
+        >
+          2
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
